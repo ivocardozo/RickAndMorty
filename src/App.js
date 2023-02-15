d304f585d4ceea6affdc7a1e114c2f621de3b63a
icarvar <ivo.cardozo@preservica.com>
2023-02-15 14:34:15 -0400

    consuming context and displaying character names

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext } from 'react';
+
+import { RickMortyContext  } from './context';
 
 function App() {
+  const appContext = useContext(RickMortyContext)
+  const { data } = appContext
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data.map(item => (
+        <li>{item.name}</li>
+      ))}
     </div>
   );
 }
