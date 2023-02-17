a44b5b5f2eb5e596e0a10a4b02fd4a4ecafa2447
icarvar <ivo.cardozo@preservica.com>
2023-02-17 14:37:03 -0400

    styling pagination component

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,18 +1,20 @@
 import React, { useContext } from 'react'
 import { RickMortyContext } from '../context'
 import { Pagination } from '@material-ui/lab'
+import classes from './Page.module.css'
 
 const Page = () => {
   const pageContext = useContext(RickMortyContext)
   const { info, fetchPage } = pageContext
 
   return (
-    <div>
-      <Pagination 
+      <Pagination
+        className={classes.pages}
+        size='large'
+        color='primary'
         count={info.pages}
         onChange={(event, page) => fetchPage(page)}
       />
-    </div>
   )
 }
 
