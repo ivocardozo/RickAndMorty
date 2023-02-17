5a3e52ef4aacdfda5d26e52cf9f6b35e90ce01af
icarvar <ivo.cardozo@preservica.com>
2023-02-17 11:28:40 -0400

    fetching data from real real api

--- src/context/index.js
+++ src/context/index.js
@@ -1,21 +1,26 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
+const url = 'https://rickandmortyapi.com/api/character'
 const RickMortyContext = React.createContext()
 
-const mockData = [
-    {
-        name: 'Rick',
-        id: '1'
-    },
-    {
-        name: 'Morty',
-        id: '2'
-    },
-]
-
 const RickMortyProvider = (props) => {
-    const [data, setData] = useState(mockData)
+    useEffect(() => {
+        fetchData(url)
+    }, [])
 
+    const [data, setData] = useState([])
+
+    const fetchData = async (url) => {
+        let query = `${url}`
+        const res = await fetch(query)
+
+        res 
+            .json()
+            .then(res => {
+                setData(res.results)
+            })
+            .catch(error => {})
+    }
     return (
     <RickMortyContext.Provider
         value={{
