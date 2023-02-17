37a856022ceb8d479a91dfca96f10c1af23e34f2
icarvar <ivo.cardozo@preservica.com>
2023-02-17 16:48:26 -0400

    create Character component

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { RickMortyContext } from '../context'
 import classes from './CharacterList.module.css'
+import Character from './Character'
 
 const CharacterList = () => {
     const dataContext = useContext(RickMortyContext)
@@ -8,7 +9,10 @@ const CharacterList = () => {
   return (
     <div className={classes.gridList}>
       {data.map(item => (
-        <li key={item.id}>{item.name}</li>
+        <Character 
+            name={item.name}
+            image={item.image}
+        />
       ))}
     </div>
   )
