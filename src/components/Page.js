860ba9d6c60fc9f078e974aeb8acabb721c61e13
icarvar <ivo.cardozo@preservica.com>
2023-02-17 17:01:07 -0400

    adding styles to pagination component

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,13 +1,17 @@
 import React, { useContext } from 'react'
 import { RickMortyContext } from '../context'
 import { Pagination } from '@material-ui/lab'
+import classes from './Page.module.css'
 
 const Page = () => {
   const dataContext = useContext(RickMortyContext)
   const {info, fetchPage} = dataContext
   return (
     <div>
-      <Pagination 
+      <Pagination
+        color='primary'
+        size='large'
+        className={classes.pages}
         count={info.pages}
         onChange={(event, page) => fetchPage(page)}
       />
